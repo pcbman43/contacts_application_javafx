0445342208c910c9ebe566dd7f62121f56bbad69
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-23 11:55:37 +0300

    Created contact deleting and data update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -89,4 +89,27 @@ public class Controller {
             data.saveContacts();
         }
     }
+
+    public void DeleteContact() {
+        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
+        if(selectedContact == null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Contacts Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select the contact you want to delete.");
+            alert.showAndWait();
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Contact");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to delete the selected contact: " + selectedContact.getFirstName() + " " + selectedContact.getLastName());
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            data.deleteContact(selectedContact);
+            data.saveContacts();
+        }
+    }
 }
